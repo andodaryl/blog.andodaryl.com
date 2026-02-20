f1972eaaae2e0533d24bb521b0c299598dcfda64
Daryl <ando.daryl@hotmail.com>
2026-02-20 20:35:46 +0000

    update site settings

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
     website: 'https://blog.andodaryl.com/', // Site domain
-    title: 'Blog | Daryl Ando', // Site title
+    title: 'blog.andodaryl', // Site title
     author: 'andodaryl', // Author name
     description: 'Blog posts from Daryl', // Site description
     language: 'en-US' // Default language
@@ -14,8 +14,8 @@ export const themeConfig: ThemeConfig = {
   general: {
     contentWidth: '35rem', // Content area width
     centeredLayout: true, // Use centered layout (false for left-aligned)
-    themeToggle: false, // Show theme toggle button (uses system theme by default)
-    postListDottedDivider: false, // Show dotted divider in post list
+    themeToggle: true, // Show theme toggle button (uses system theme by default)
+    postListDottedDivider: true, // Show dotted divider in post list
     footer: true, // Show footer
     fadeAnimation: true // Enable fade animations
   },
@@ -29,7 +29,7 @@ export const themeConfig: ThemeConfig = {
 
   // POST SETTINGS ///////////////////////////////////////////////////////////////////////////////////////
   post: {
-    readingTime: false, // Show reading time in posts
+    readingTime: true, // Show reading time in posts
     toc: true, // Show table of contents (when there is enough page width)
     imageViewer: true, // Enable image viewer
     copyCode: true, // Enable copy button in code blocks
